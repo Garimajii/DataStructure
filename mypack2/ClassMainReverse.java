df0f6d52554e4156c8563fec31dad427ae48677f
Garima Varshney <141229671+Garimajii@users.noreply.github.com>
2024-09-26 00:07:05 +0530

    Add files via upload

---
+++ mypack2/ClassMainReverse.java
@@ -0,0 +1,13 @@
+package mypack2;
+
+public class ClassMainReverse {
+
+	public static void main(String[] args) {
+		
+		int[] arg= {1,4,53,5,4,3,224,35};
+		System.out.println(ReverseArray.doReverse(arg));
+		
+
+	}
+
+}
---
+++ mypack2/ReverseArray.java
@@ -0,0 +1,14 @@
+package mypack2;
+
+public class ReverseArray {
+	
+	public static int doReverse(int[] args)
+	{
+		for(int i=args.length-1;i>=0;i--)
+		{
+			System.out.println(args[i]);
+		}
+		return -1;
+	}
+
+}
